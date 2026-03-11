2d6d148208d11016495ef157dc6c322e87cce174
solitude000k <80776202+solitude000k@users.noreply.github.com>
2026-03-11 21:10:55 +0900

    送信先URLを変更。

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-window.KUSHIRO_API_BASE   = 'https://kushiro-chat-auth.solitude000k.workers.dev';
+window.KUSHIRO_API_BASE   = 'https://kushiro-chat-auth.preview-me.workers.dev';
 window.KUSHIRO_API_SECRET = 'Solitude1137';
